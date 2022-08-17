56cc5dd159ba0163bdb2c5993108d074714613c8
Iggy <lbvillapando@up.edu.ph>
2022-08-18 00:49:33 +0800

    added the upload page and the initial upload code

--- app/website/templates/tableau.js
+++
@@ -1,6 +0,0 @@
-function initViz() {
-    var containerDiv = document.getElementById("vizContainer"),
-    url = "https://public.tableau.com/views/EmbedTest_16606360562240/ProfitDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link";
-
-    var viz = new tableau.Viz(containerDiv, url);
-}
--- app/website/views.py
+++ app/website/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, current_app, Flask
 from flask_login import login_required, current_user
+
+import os
 
 views = Blueprint('views', __name__)
 
@@ -7,3 +9,22 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+# current_app.config["FILE_UPLOADS"] = r"app\website\static\uploads"
+
+
+@views.route('/upload', methods=["GET", "POST"])
+@login_required
+def upload():
+
+    if request.method=="POST":
+        if request.files:
+            file = request.files["dataset"]
+            print(file)
+            # file.save(os.path.join(current_app.config["FILE_UPLOADS"], file.filename))
+            # file.save(os.path.join('static/uploads', file.filename))
+
+            print("image saved")
+            return redirect(request.url)
+
+    return render_template("upload.html", user=current_user)
